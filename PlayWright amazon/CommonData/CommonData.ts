08c72b373bd00e275fe433fb343eb8096691e74d
Ashish yadav <ay9054252@gmail.com>
2026-08-17 11:14:15 +0530

    new latest updated Commit

---
+++ PlayWright amazon/CommonData/CommonData.ts
@@ -0,0 +1,44 @@
+
+export let CommonData = {
+
+    HitUrl: {
+
+        Url: "http://localhost:8888/"
+    },
+
+    Login: {
+
+        userName: "admin",
+        password: "admin"
+    },
+
+    leadData: {
+
+        nameTitle: "Mr.",
+        firstName: "Ashish",
+        lastName: "YDV",
+        companyName: "Enixo Corporate PVT LTD..",
+        titleName: "QA",
+        LeadSource: 3,
+        IndustryName: 2,
+        AnnualRevenue: "80,000",
+        NoOfEmployees: "12",
+        SecondaryEmail: "ashishkumaryadav@gmail.com",
+        phoneNum: "9877654556",
+        mobileNum: "5463723456",
+        faxnum: "ARD8956",
+        emailID: "ay9054252@gmail.com",
+        websiteNmae: "www.aky.com",
+        leadStatus: 3,
+        rating: 2,
+        street: "Chauri",
+        postalCode: "221309",
+        country: "INDIA",
+        pOBox: "221402",
+        city: "BHADOHI",
+        state: "UP",
+        description: "QA Automation"
+    }
+
+    
+}
